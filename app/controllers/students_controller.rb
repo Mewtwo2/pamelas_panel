ab4d79cc7b757fdf28c0cf00d157f05535038f1b
Ibteda Sabur <akkount35@gmail.com>
2018-09-09 23:59:06 -0400

    added functionality to check against cohort db for already existing student ids

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -33,17 +33,35 @@ class StudentsController < ApplicationController
 
     if params[:cohort_id]
       @cohort = Cohort.find(params[:cohort_id])
-
       if @cohort.student_ids == nil
         @cohort.student_ids = []
         @cohort.student_ids << params[:id]
         @cohort.save
       else
-        @cohort.student_ids << params[:id]
-        @cohort.save
-      end
-    end
-  end
+        @cohort.student_ids.each do |student|
+
+          if student.to_i == params[:id].to_i
+              p "This student is already in the Cohort"
+          end
+
+          @cohort.student_ids << params[:id].to_i
+          @cohort.student_ids.uniq! # Could not figure out why my other if statement conditional was failing
+          @cohort.save
+
+        end # Ends Student ID Iteration Loop
+      end # Ends Student ID Array Nil If Statement Check
+
+      # @cohort_students = []
+      #
+      # @cohort.student_ids.each do |student|
+      #   @cohort_students << Student.find(student)
+      # end
+      #
+      # p "Students"
+      # p @cohort_students
+      redirect_to @cohort
+    end # Ends URL Check
+  end # Ends Action
 
   def index
     redirect_to root_path unless logged_in?
