1acb0640528078bb2e97d2abed07756d851ace19
Ibteda Sabur <akkount35@gmail.com>
2018-09-15 20:48:00 -0400

    added validations on the back end for admin sign up

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,9 @@
 class Admin < ApplicationRecord
   before_save {self.username = username.downcase}
   has_secure_password
-  validates :username, presence: true
-  validates :password, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: {minimum: 8,maximum:20}
+  validates :admin_key, presence: true, length: {is: 6}, numericality: {only_integer: true}
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
